5ede5fa263ea8852ae557dd1eb4ec51d82488b18
oku-44 <55417512+oku-44@users.noreply.github.com>
2023-12-30 18:10:01 +0900

    fix: add header and modify config (#2)

---
+++ app/ui/navbar.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link"
+
+type Link = {
+  name: string
+  icon: string
+  href: string
+}
+
+const links = [
+  { name: "Home", icon: "home", href: "/" },
+  { name: "Sponsor", icon: "home", href: "/sponsor" },
+  { name: "About", icon: "home", href: "/about" },
+  { name: "Contact", icon: "home", href: "/contact" },
+]
+
+export default function NavBar() {
+  return(
+  <div className="drawer drawer-end">
+    <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
+    {/* Navbar */}
+    <div className="drawer-content flex flex-col">
+      <div className="w-full navbar bg-base-300">
+        <div className="flex-1 px-2 mx-2">TsKaigi</div>
+        <div className="flex-none hidden lg:block">
+          <ul className="menu menu-horizontal">
+            {links.map((link: Link) => (
+              <li>
+                <Link href={link.href}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex-none lg:hidden">
+          <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          </label>
+        </div>
+      </div>
+    </div> 
+    {/* Sidebar */}
+    <div className="drawer-side">
+      <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
+      <ul className="menu p-4 w-80 min-h-full bg-base-200">
+        {/* Sidebar content here */}
+        {links.map((link: Link) => (
+              <li>
+                <Link href={link.href}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+      </ul>
+    </div>
+  </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,15 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["aqua"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 }
 export default config
