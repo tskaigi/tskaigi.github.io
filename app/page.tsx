b55b7e974f0065a1ef34bea34b0d3ca00a876368
niwa-takeru <33322961+niwa-takeru@users.noreply.github.com>
2024-02-29 11:01:51 +0900

    オプショナルプランのレイアウトをシルバー相当に揃える

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Container from "./ui/container";
 import { HiExternalLink } from "react-icons/hi";
 import { NavigateAnchorOnFirstRender } from "./functional/navigate-anchor-on-first-render";
-import { SponsorList } from "./lib/definitions";
+import { SponsorData, SponsorList } from "./lib/definitions";
 import { sponsorList } from "./lib/data";
 
 const pageAnchors = {
@@ -11,6 +11,49 @@ const pageAnchors = {
   sponsor: 'sponsor',
 }
 
+const sponsorVariants = {
+  Platinum: {
+    divider: `text-secondary divider-secondary`,
+    gridSize: `gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3`,
+    height: `h-48`,
+  },
+  Gold: {
+    divider: `text-accent divider-accent`,
+    gridSize: `gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4`,
+    height: `h-36`,
+  },
+  Silver: {
+    divider: `text-primary divider-primary`,
+    gridSize: `gap-2 grid-cols-3 sm:grid-cols-4 md:grid-cols-5`,
+    height: `h-28`,
+  },
+  Bronze: {
+    divider: `text-error divider-error`,
+    gridSize: `gap-1 grid-cols-4 sm:grid-cols-5 md:grid-cols-6`,
+    height: `h-24`,
+  },
+  Refreshment: {
+    divider: `text-gray-500 divider-gray-500`,
+    gridSize: `gap-4 grid-cols-1`,
+    height: `h-28`,
+  },
+  Coffee: {
+    divider: `text-gray-500 divider-gray-500`,
+    gridSize: `gap-4 grid-cols-1`,
+    height: `h-28`,
+  },
+  Beer: {
+    divider: `text-gray-500 divider-gray-500`,
+    gridSize: `gap-4 grid-cols-1`,
+    height: `h-28`,
+  },
+  "Event Platform": {
+    divider: `text-gray-500 divider-gray-500`,
+    gridSize: `gap-4 grid-cols-1`,
+    height: `h-28`,
+  },
+} as const;
+
 export default function Home({
   searchParams
 }: {
@@ -18,56 +61,6 @@ export default function Home({
 }) {
   const showSponsorDraft = typeof searchParams.sponsorDraft !== "undefined"
   const plans: (keyof SponsorList)[] = Object.keys(sponsorList) as (keyof SponsorList)[];
-  const sponsorVariants = {
-    Platinum: {
-      divider: `text-secondary divider-secondary`,
-      gridSize: `gap-4 gridcols-1 sm:grid-cols-2 md:grid-cols-3`,
-      height: `h-48`,
-      colStart: ``,
-    },
-    Gold: {
-      divider: `text-accent divider-accent`,
-      gridSize: `gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4`,
-      height: `h-36`,
-      colStart: ``,
-    },
-    Silver: {
-      divider: `text-primary divider-primary`,
-      gridSize: `gap-2 grid-cols-3 sm:grid-cols-4 md:grid-cols-5`,
-      height: `h-28`,
-      colStart: ``,
-    },
-    Bronze: {
-      divider: `text-error divider-error`,
-      gridSize: `gap-1 grid-cols-4 sm:grid-cols-5 md:grid-cols-6`,
-      height: `h-24`,
-      colStart: ``,
-    },
-    Refreshment: {
-      divider: `text-gray-500 divider-gray-500`,
-      gridSize: `gap-4 gridcols-1 sm:grid-cols-2 md:grid-cols-3`,
-      height: `h-36`,
-      colStart: `sm:col-start-2`,
-    },
-    Coffee: {
-      divider: `text-gray-500 divider-gray-500`,
-      gridSize: `gap-4 gridcols-1 sm:grid-cols-2 md:grid-cols-3`,
-      height: `h-36`,
-      colStart: `sm:col-start-2`,
-    },
-    Beer: {
-      divider: `text-gray-500 divider-gray-500`,
-      gridSize: `gap-4 gridcols-1 sm:grid-cols-2 md:grid-cols-3`,
-      height: `h-36`,
-      colStart: `sm:col-start-2`,
-    },
-    "Event Platform": {
-      divider: `text-gray-500 divider-gray-500`,
-      gridSize: `gap-4 gridcols-1 sm:grid-cols-2 md:grid-cols-3`,
-      height: `h-36`,
-      colStart: `sm:col-start-2`,
-    },
-  };
 
   return (
     <>
@@ -151,43 +144,79 @@ export default function Home({
             </h2>
           </div>
           <div className="mx-auto pb-20">
-            {plans.map((plan) => {
+            {(["Platinum", "Gold", "Silver"] as const).map((plan) => {
               const list = sponsorList[plan].filter(sponsor => showSponsorDraft || !sponsor.draft)
               if (list.length === 0) {
                 return null
               }
               return (
-              <div key={plan}>
-                <h3 className={`text-2xl font-bold divider ${sponsorVariants[plan].divider}`}>
-                  {plan} Sponsors
-                </h3>
-                <div className={`grid my-8 px-4 sm:px-6 lg:px-8 mt-8 mb-12 grid-cols-1 ${sponsorVariants[plan].gridSize}`}>
-                  {list.map((sponsor) => (
-                    <Link
-                      key={sponsor.name}
-                      href={sponsor.logoLink}
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      className={`${sponsorVariants[plan].colStart}`}
-                      >
-                      <div
-                        key={sponsor.name}
-                        className={`text-sm my-auto rounded-md md:rounded-lg bg-white mb-4 hover:opacity-80 hover:shadow-md transition duration-300 ease-in-out overflow-hidden ${sponsorVariants[plan].height} ${plan === "Bronze" ? (sponsor.addPadding ? "p-2" : "p-1") : (sponsor.addPadding ? "p-8" : "p-2")}`}
-                      >
-                        <img
-                          className="p-4 h-full w-full object-contain"
-                          src={sponsor.logoImage}
-                          alt={sponsor.name}
-                        />
-                      </div>
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            )})}
+                <PlanSponsors
+                  plan={plan}
+                  sponsors={list}
+                />
+              )}
+            )}
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 gap-4">
+              {(["Event Platform", "Refreshment", "Coffee", "Beer"] as const).map(plan => {
+                const list = sponsorList[plan].filter(sponsor => showSponsorDraft || !sponsor.draft)
+                if (list.length === 0) {
+                  return null
+                }
+                return (
+                  <PlanSponsors
+                    plan={plan}
+                    sponsors={list}
+                    isOptionalPlan
+                  />
+                )}
+              )}
+            </div>
+            <PlanSponsors
+              plan="Bronze"
+              sponsors={sponsorList.Bronze}
+            />
           </div>
         </div>
       )}
     </>
   );
+}
+
+const PlanSponsors = ({
+  plan,
+  sponsors,
+  isOptionalPlan
+}: {
+  plan: keyof SponsorList;
+  sponsors: SponsorData[]
+  isOptionalPlan?: boolean
+}) => {
+  return (
+    <div key={plan}>
+      <h3 className={`text-2xl font-bold divider ${sponsorVariants[plan].divider}`}>
+        {`${plan}${!isOptionalPlan ? " Sponsors" : ""}`}
+      </h3>
+      <div className={`grid my-8 px-4 sm:px-6 lg:px-8 mt-8 mb-12 grid-cols-1 ${sponsorVariants[plan].gridSize}`}>
+        {sponsors.map((sponsor) => (
+          <Link
+            key={sponsor.name}
+            href={sponsor.logoLink}
+            rel="noopener noreferrer"
+            target="_blank"
+            >
+            <div
+              key={sponsor.name}
+              className={`text-sm my-auto rounded-md md:rounded-lg bg-white mb-4 hover:opacity-80 hover:shadow-md transition duration-300 ease-in-out overflow-hidden ${sponsorVariants[plan].height} ${["Platinum", "Gold"].includes(plan) ? (sponsor.addPadding ? "p-8" : "p-2") : (sponsor.addPadding ? "p-2" : "p-1")}`}
+            >
+              <img
+                className="p-4 h-full w-full object-contain"
+                src={sponsor.logoImage}
+                alt={sponsor.name}
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
