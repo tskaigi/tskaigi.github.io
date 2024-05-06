2e9210ee5c4d871ef4fba8a8c4e52c4e8e4ad163
takezoux2 <takezoux2@gmail.com>
2024-05-06 14:27:05 +0900

    項目追加

--- app/specific-commercial/page.tsx
+++ app/specific-commercial/page.tsx
@@ -34,24 +34,28 @@ export default function SpecifitCommercialTopPage() {
             <td>竹下義晃</td>
           </tr>
           <tr>
-            <td>追加手数料等の追加料金</td>
-            <td>※なお、インターネット接続料金その他の電気通信回線の通信に関する費用はお客様にて別途ご用意いただく必要があります（金額は、お客様が契約した各事業者が定める通り）。</td>
+            <td>販売価格</td>
+            <td><a className=" text-yellow-100" href="https://client.eventhub.jp/ticket/VjqcjZK60">チケット販売ページ</a>をご確認ください。</td>
           </tr>
           <tr>
-            <td>交換および返品（返金ポリシー）</td>
-            <td>お客様都合により参加をキャンセルする場合は、EventHub上からキャンセルください。所定の手数料を引いたうえでご返金いたします。</td>
+            <td>受け付け可能な決済手段</td>
+            <td>クレジットカード</td>
           </tr>
           <tr>
             <td>代金の支払時期</td>
             <td>ご利用のクレジットカードの締め日や契約内容により異なります。ご利用されるカード会社までお問い合わせください。</td>
           </tr>
           <tr>
-            <td>販売価格</td>
-            <td><a className=" text-yellow-100" href="https://client.eventhub.jp/ticket/VjqcjZK60">チケット販売ページ</a>をご確認ください。</td>
-          </tr>
-          <tr>
             <td>引渡時期</td>
             <td>2024/5/11 TSKaigi2024</td>
+          </tr>
+          <tr>
+            <td>交換および返品（返金ポリシー）</td>
+            <td>お客様都合により参加をキャンセルする場合は、EventHub上からキャンセルください。所定の手数料を引いたうえでご返金いたします。</td>
+          </tr>
+          <tr>
+            <td>追加手数料等の追加料金</td>
+            <td>※なお、インターネット接続料金その他の電気通信回線の通信に関する費用はお客様にて別途ご用意いただく必要があります（金額は、お客様が契約した各事業者が定める通り）。</td>
           </tr>
         </table>
       </Container>
