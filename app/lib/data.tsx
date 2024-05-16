c3810dedd38730d5563ef0414935727b875e9b0d
Niwa Takeru <33322961+niwa-takeru@users.noreply.github.com>
2024-05-16 12:04:43 +0900

    After Talkイベントを追加 (#153)

--- app/lib/data.tsx
+++ app/lib/data.tsx
@@ -905,5 +905,22 @@ export const SUB_EVENTS = [
     day: "金",
     link: "https://progate.connpass.com/event/318009/",
     thumbnail: "/subevent_5_17.png",
+  },
+  {
+    title: "TSKaigi 2024 After Talk",
+    description: `TSKaigi本編に引き続き、TypeScriptに関する発表の場を提供するとともに、TSKaigiコミュニティの活性化を図ることを目的としています。
+
+TSKaigi 2024にご参加された方はもちろん、参加できなかった方、プロポーザルが惜しくも採択されなかった方も、ぜひこの機会にご参加ください。
+
+本編の熱気に続き、セッションやLTを通してTypeScriptの知見を共有し、そして懇親会では横のつながりを深めましょう！
+
+オフライン・オンライン問わず、皆様のご参加をスタッフ一同心よりお待ちしております。
+`,
+    sponsorList: ["株式会社LayerX", "株式会社Helpfeel", "アセンド株式会社"],
+    tags: ["公募枠あり", "オンラインあり"],
+    date: "6/4",
+    day: "火",
+    link: "https://layerx.connpass.com/event/318123/",
+    thumbnail: "/subevent_6_4.png",
   }
 ];
